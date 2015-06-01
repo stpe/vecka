e21562bebc73d3b082684575aac2b9fc21ac8dd4
Stefan Pettersson <stefpet@gmail.com>
2015-06-01 08:15:44 +0200

    Just a tweak riding the subway...

--- js/vecka.js
+++ js/vecka.js
@@ -1,18 +1,42 @@
-var week = document.getElementById('week');
-var period = document.getElementById('period');
+(function () {
+    var week = document.getElementById('week');
+    var period = document.getElementById('period');
 
-moment.locale('sv');
+    moment.locale('sv');
 
-var now = moment();
+    var now = moment();
 
-week.value = now.week();
-update(now);
+    week.value = now.week();
+    update(now);
 
-week.addEventListener('change', function(e) {
-    console.log(week.value);
-    update( moment().week(week.value) );
-});
+    // update period when input field change
+    week.addEventListener('change', function(e) {
+        if (week.value == "") {
+            // if empty, default to this week
+            week.value = now.week();
+        }
 
-function update(m) {
-    period.innerHTML = m.startOf('week').format("D MMM") + ' - ' + m.endOf('week').format("D MMM");
-}
+        update( moment().week(week.value) );
+    });
+
+    // select all when field clicked
+    week.addEventListener('click', function(e) {
+        week.select();
+    });
+
+    // only allow digits to be entered
+    week.addEventListener('keypress', function(e) {
+        var key = event.which || event.keyCode;
+        if (key == 13) {
+            // return key
+            return true;
+        } else if (key < 48 || key> 57) {
+            // something else than a digit
+            e.preventDefault();
+        }
+    });
+
+    function update(m) {
+        period.innerHTML = m.startOf('week').format("D MMM") + ' - ' + m.endOf('week').format("D MMM");
+    }
+})();
